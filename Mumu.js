25e70754fd505287666c5cd2aa9387a299524871
wwdc14 <wwdc14@vip.qq.com>
2024-03-21 22:53:36 +0800

    Update Mumu.js

--- Mumu.js
+++ Mumu.js
@@ -1,8 +1,28 @@
-let body = JSON.parse($response.body);
-body.data.nickname = "Test";
-body.data.current_device.trial_end_at = 1742436947;
-body.data.current_device.trial_status = 1;
-console.log("body: ", body);
-const bodyString = JSON.stringify(body);
-console.log("bodyString: ", bodyString);
-$done({ bodyString });
+// let body = JSON.parse($response.body);
+// body.data.nickname = "Test";
+// body.data.current_device.trial_end_at = 1742436947;
+// body.data.current_device.trial_status = 1;
+// console.log("body: ", body);
+// const bodyString = JSON.stringify(body);
+// console.log("bodyString: ", bodyString);
+// $done({ bodyString });
+
+// const body = JSON.stringify({
+//     "code": 0,
+//     "msg": "ok",
+//     "data": {
+//         "user_id": "aebglmpozaaaabua",
+//         "nickname": "fhk",
+//         "member_expired_at": 7,
+//         "member_status": 1,
+//         "enabled_device_count": 1,
+//         "current_device_status": 1,
+//         "current_device": {
+//             "device_id": "aeawlmporaaaabvm",
+//             "alias": "fhk的MacBook Pro",
+//             "last_binded_at": 1710418935,
+//             "trial_end_at": 17078399990,
+//             "trial_status": 1
+//         }
+//     }
+// })
