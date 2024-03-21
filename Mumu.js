458c3cb9a2c97c3a5bbbf3e578275aab40c96a57
wwdc14 <wwdc14@vip.qq.com>
2024-03-21 22:40:05 +0800

    Update Mumu.js

--- Mumu.js
+++ Mumu.js
@@ -1,20 +1,8 @@
-const modifyResponse = (response) => {
-  try {
-    let data = JSON.parse(response.body);
-    data.data.nickname = "Test";
-    if (data && data.data && data.data.current_device) {
-      data.data.current_device.trial_end_at = 1742436947;
-      data.data.current_device.trial_status = 1;
-    }
-    console.log(data)
-    console.log(JSON.stringify(data))
-    return JSON.stringify(data);
-  } catch (e) {
-    console.error("Error modifying response", e);
-  }
-  return response.body;
-};
-const body = modifyResponse($response);
-console.log("body: ", body)
-console.log($response.body)
-$done({ body });
+let body = JSON.parse($response.body);
+body.data.nickname = "Test";
+body.data.current_device.trial_end_at = 1742436947;
+body.data.current_device.trial_status = 1;
+console.log("body: ", body);
+const bodyString = JSON.stringify(body);
+console.log("bodyString: ", bodyString);
+$done({ bodyString });
